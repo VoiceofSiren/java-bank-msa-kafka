5ef6ddc807943f8945cc926fb5f3b75c4bcdfef3
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-07-25 11:01:15 +0900

    first commit

---
+++ src/main/java/org/example/bank/bankroot/BankRootApplication.java
@@ -0,0 +1,13 @@
+package org.example.bank.bankroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankRootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bank/bankroot/BankRootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bank.bankroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
